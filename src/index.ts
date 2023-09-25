c810f291f1544842bd62923c42302e2f89724e3a
shuchengc <cuishucheng@gmail.com>
2023-09-26 07:20:07 +1000

    chore: add command reminder

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,14 @@ import Command from './models/command';
 import { WIDTH, HEIGHT } from './consts';
 
 const table = new Table(WIDTH, HEIGHT);
-const command = new Command(table)
+const command = new Command(table);
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false
-})
+});
 
 rl.on('line', input => {
   command.run(input);
-})
+});
+console.log('Please input a command:');
